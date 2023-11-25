6665cff2355c987c886f4a91f84d5019639d4f08
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-11-25 03:53:02 -0500

    refactor, add some links to nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import MainContent from "./components/MainContent";
 import NavBar from "./components/NavBar";
 import "./styles/main.scss";
 import Footer from "./components/Footer";
+import MainContainer from "./components/main/MainContainer";
+import SideBar from "./components/sidebar/SideBar";
 
 function App() {
   return (
     <div id="homepage-container">
       <NavBar />
-      <MainContent />
+      <div id="main-content">
+        <SideBar />
+        <MainContainer />
+      </div>
       <Footer />
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,8 @@ function NavBar() {
   return (
     <nav id="nav-bar">
       <img src={logo} alt="logo" onClick={() => (window.location.href = "/")} />
+      <a href="https://www.linkedin.com/in/jonzav/">Linkedin</a>
+      <a href="https://github.com/JonZavialov">GitHub</a>
       <div id="search-bar">
         <span>🔎︎</span> My Portfolio
       </div>
