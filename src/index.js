292db81eb6fa9cb8e467f115976f654487176111
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2026-01-18 20:20:49 -0500

    Add posthog analytics

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: process.env.REACT_APP_POSTHOG_HOST,
+  defaults: "2025-11-30",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_POST_HOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>,
 );
